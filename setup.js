a5468e60d13668cd27f863cb8f011fae405eec53
jesuskata <rzarate.alex@gmail.com>
2020-01-09 11:59:27 -0600

    Added: Included the setup file for the hbs template

---
+++ setup.js
@@ -0,0 +1,9 @@
+// setup.js
+module.exports = function (Handlebars) {
+  Handlebars.registerHelper('custom', function (context, options) {
+    return 'custom helpers!'
+  })
+}
+
+// custom-template.hbs
+Now you can use {{custom}}
